dd783ff0fb3e07954f5d9b96257979e73c275401
garnkai <garn0092@algonquinlive.com>
2024-11-22 10:37:28 -0500

    adding constants

---
+++ TP.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ofMain.h"
+
+namespace TP {
+
+    static const int APP_WINDOW_WIDTH = 1200;
+    static const int APP_WINDOW_HEIGHT = 740;
+    static const int APP_DESIRED_FRAMERATE = 60;
+
+    static const std::string BITING1_PATH_COLOUR = "biting/moodengBite1.png";
+    static const std::string BITING2_PATH_COLOUR = "biting/moodengBite2.png";
+    static const std::string RESTING1_PATH_COLOUR = "resting/moodengRestBig.png";
+    static const std::string RESTING2_PATH_COLOUR = "resting/moodengRest2Big.png";
+    static const std::string SLEEPING1_PATH_COLOUR = "sleeping/sleeping1Big.png";
+    static const std::string SLEEPING2_PATH_COLOUR = "sleeping/sleeping2Big.png";
+    static const std::string SLEEPING3_PATH_COLOUR = "sleeping/sleeping3Big.png";
+
+    static const std::string WATERMELON_UID = "DC AD 84 33";
+    static const std::string WATER_UID = "EC D1 7C 32";
+
+    static const int PHOTO_WIDTH = 320;
+    static const int PHOTO_HEIGHT = 240;
+
+
+
+}
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "TP.h"
 
 class ofApp : public ofBaseApp{
 
